36875ca5215638a22956dd29155ba519b2f64b2f
borrillis <cummings.michael@gmaiul.com>
2011-01-25 13:39:18 -0500

    Added Common.Logging

---
+++ Dependencies/Common.Logging/Source/package/src/Common.Logging.NLog/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+using System.Security;
+
+[assembly: AssemblyProduct("Common Logging Framework NLog 1.0.0.505 Adapter")]
+[assembly: SecurityTransparent]
---
+++ Dependencies/Common.Logging/Source/src/Common.Logging.EntLib41/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+using System.Security;
+
+[assembly: AssemblyProduct("Common Logging Framework Enterprise Library 4.1 Adapter")]
+[assembly: SecurityTransparent]
