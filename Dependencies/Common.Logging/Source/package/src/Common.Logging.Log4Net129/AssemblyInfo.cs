1de6eaaa32e2b0901440b53d9ef451dcf4009190
Michael Cummings <cummings.michael@live.com>
2016-06-04 09:16:18 -0400

    Removed checked in binary dependencies
    --HG--
    branch : VS2015Upgrade

--- Dependencies/Common.Logging/Source/package/src/Common.Logging.Log4Net129/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Reflection;
-using System.Security;
-
-[assembly: AssemblyProduct("Common Logging Framework log4net 1.2.9 Adapter")]
-[assembly: SecurityTransparent]
--- Dependencies/Common.Logging/Source/src/Common.Logging.EntLib/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Reflection;
-using System.Security;
-
-[assembly: AssemblyProduct("Common Logging Framework Enterprise Library 3.1 Adapter")]
-[assembly: SecurityTransparent]
